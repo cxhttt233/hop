8fec950efffe775ff50b4d8105c6743aa305a199
LinkSpider <cxhttt@qq.com>
2026-09-23 21:25:49 +0800

    chore: fix ASF header on config JSON codec

--- core/src/main/java/org/apache/hop/metadata/serializer/json/ConfigJsonSerializer.java
+++ core/src/main/java/org/apache/hop/metadata/serializer/json/ConfigJsonSerializer.java
@@ -2,7 +2,17 @@
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.hop.metadata.serializer.json;
 
@@ -121,8 +131,6 @@ public final class ConfigJsonSerializer {
         continue;
       }
       if (property.storeWithName()) {
-        // Name references require the session metadata provider to resolve safely. Keep this first
-        // core slice lossless in the outbound direction and reject inbound ambiguity explicitly.
         throw new HopException("Config property '" + key + "' is a named metadata reference");
       }
       set(target, field, readValue(node, field, property));
